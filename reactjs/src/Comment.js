5f8aac2320991a09489efb1b158066239a7dd68d
Manpreet Singh <manpreet@digined.com>
2015-04-18 17:34:46 +0200

    jammin with reactjs

---
+++ reactjs/src/Comment.js
@@ -0,0 +1,13 @@
+var Comment = React.createClass({displayName: "Comment",
+  render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return (
+      React.createElement("div", {className: "comment"}, 
+        React.createElement("h2", {className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.createElement("span", {dangerouslySetInnerHTML: {__html: rawMarkup}})
+      )
+    );
+  }
+});
