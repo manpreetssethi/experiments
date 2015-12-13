c9fcadb43d3c449a8fc98afa7eb11763908c7322
Manpreet Singh <manpreet@digined.com>
2015-12-14 00:27:30 +0100

    a change in the app test

--- todo-mvc/tests/app-tests.js
+++ todo-mvc/tests/app-tests.js
@@ -3,12 +3,12 @@
  */
 define(
     [
-        'podlists/app/App',
+        'todomvc/app/todoMVCApp',
         'backbone'
     ],
     function(
         App,
-        Backbone
+        todoMVCApp
     ){
         var should = require('chai').should();
         var expect = require('chai').expect;
@@ -16,12 +16,8 @@ define(
 
         describe('App', function() {
             beforeEach(function() {
-                this.app = new App({});
+                this.app = todoMVCApp;
             });
-
-            it('should have a property "options" assigned with the first argument in the contstructor', function(){
-                expect(this.app.options).to.eql({});
-            })
 
             it('should start backbone history when startHistory is called', function(){
                 this.historyStartStub = sinon.stub(Backbone.history, 'start');
