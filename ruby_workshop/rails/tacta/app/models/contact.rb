955b7164676404ac649f1b5ea33fe56626a69960
Manpreet <manpreet@wetransfer.com>
2018-01-13 15:19:41 +0100

    added validation

--- ruby_workshop/rails/tacta/app/models/contact.rb
+++ ruby_workshop/rails/tacta/app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :phone, allow_blank: true, length: { in: 5..20 }
+  validates :email, allow_blank: true, length: { in: 5..50 }
+
+  validates_format_of :phone, allow_blank: true, :with => /\A[+ 0-9]+$\z/
+  validates_format_of :email, allow_blank: true, :with => /@/, message: 'must contain @.'
 end
